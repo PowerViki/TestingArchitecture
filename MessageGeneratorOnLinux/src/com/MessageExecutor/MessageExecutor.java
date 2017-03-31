7e59a19d0ab0b87d6892de9f0b46674e05e5cb88
李赟 <yun.li@sumscope.com>
2017-03-31 17:59:23 +0800

    update the ReadBigFile class

--- MessageGeneratorOnLinux/src/com/MessageExecutor/MessageExecutor.java
+++ MessageGeneratorOnLinux/src/com/MessageExecutor/MessageExecutor.java
@@ -1,25 +1,27 @@
 package com.MessageExecutor;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.io.RandomAccessFile;
+
 
 
 public class MessageExecutor {
-	//public final static String filepath = "D:/50041.txt";
-    public final static String filepath = "/home/sumscope/Gate.Monitor.V5.log/Gate.Monitor.V5.log";
-    public final static String messageid = "50041";
+	//public final static String filepath = "F:/Serverlogaa.txt";
+    public final static String filepath = "/home/sumscope/Gate.Monitor.V5.log/GateMonitorV5_20161129.log";
+    public final static String messageid = "55020";
+    
 	public static void main(String[] args) {
 		ReadBigFile myreader = new ReadBigFile(filepath);
-		myreader.setMessageid(messageid);
-		try {
-			myreader.readFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String msglist[] = new String[]{"50330","50666","50255","50049","55300","50048","50658","50659","50620","50667","61160","50253"};
+		for(int i=0;i<msglist.length;i++){
+			myreader.setMessageid(msglist[i]);
+			try {
+				myreader.readFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
 		
     }
 }
--- MessageGeneratorOnLinux/src/com/MessageExecutor/ReadBigFile.java
+++ MessageGeneratorOnLinux/src/com/MessageExecutor/ReadBigFile.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReadBigFile {
 	private String filepath;//= "D:/50041.txt";
-	private String tempstr;
+	private String tempstr="";
     private char[] buffer = new char[1024*1024*3];
     private int byteread = 0;
     private int start;
     private int end;
     private String messageid;
+    private int i;
     
     public ReadBigFile(){
     }
@@ -42,31 +42,64 @@ public class ReadBigFile {
 		  //randomFile = new RandomAccessFile(filepath, "r");
 		 // long fileLength = randomFile.length();
 		  BufferedReader cpreader = new BufferedReader(new InputStreamReader(new FileInputStream(this.getFilePath())));
-		  while((byteread=cpreader.read(buffer))!=-1){
+		  loop1:while((byteread=cpreader.read(buffer))!=-1){
 	          String str = new String(buffer,0,byteread);
+	          
 			  Matcher start_line = Pattern.compile("\\[\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\]\\[Type\\]\\("+this.getMessageid()+"\\)").matcher(str);
 			  
 			  Matcher end_line = Pattern.compile("\\[\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\]msg_size = \\d{1,}").matcher(str);
-	          
-			 Matcher response_line = Pattern.compile("\\[\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\]<MAP>\\[AckMsgHead\\]").matcher(str);
 			  
-			  loop1:while(start_line.find()){
-				  loop2:while(response_line.find()){
+	          
+              Matcher response_line = Pattern.compile("\\[\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\].*\\[AckMsgHead\\]").matcher(str);
+              i=0;
+              
+              loop2:while(start_line.find()){
+            	  
+            	  if (end_line.find(start_line.start(i))){
+            		  start = start_line.start(i);
+            		  end = end_line.end();
+            		  response_line.region(start, end);
+            		  if (response_line.find()){
+            			  end_line.reset();
+            			  response_line.reset();
+            			  continue loop2;
+            		  }else{
+            			  tempstr = new String(buffer,start,end-start);
+            			  end_line.reset();
+            			  response_line.reset();
+            			  start_line.reset();
+            			  break loop1;
+            		  }
+            	  }else{
+            		  end_line.reset();
+        			  response_line.reset();
+            	  }
+            		 i = i+1;
+              }
+              start_line.reset();
+            	  
+            	  
+			 /* loop1:while(start_line.find()){
+				  i = i+1;
+				  loop2:while(response_line.find(start_line.start(i))){
+					  response_line.reset();
 					  continue loop1;
 				  }
-				 // randomFile.seek(start);
-			     // System.out.println(start_line.group());
-				  loop3:while(end_line.find()){
-					  start=start_line.start();
+				  loop2:while(end_line.find(start_line.start(i))){
+					  start=start_line.start(i);
 					  end = end_line.end();
 					  tempstr = new String(buffer,start,end-start);
 					  //System.out.println(end_line.group());
+	
 					  break loop1;
 				  }
-			  }
+				  
+			  }*/
+
 		  }
+		
 		  cpreader.close();
-		  if (tempstr!=null){
+		  if (tempstr.length()>0){
 			  MessageFileWrite write = new MessageFileWrite(this.getFilePath(),this.getMessageid());
 			  write.writefile(tempstr);
 		  }
